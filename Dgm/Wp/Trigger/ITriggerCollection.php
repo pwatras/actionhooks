ebf7378336b5fd84f103acb7fa32c96268edf555
igrek <p.watras@dgmonitor.pl>
2019-12-25 21:47:14 +0100

    cleanup: remove commented code

--- Dgm/Wp/Trigger/ITriggerCollection.php
+++ Dgm/Wp/Trigger/ITriggerCollection.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Dgm\Wp\Trigger;
-//use Dgm\Wp\ITrigger;
+
 interface ITriggerCollection /*extends \Iterator*/ {
     //public function current():ITrigger;
     public function attachAll();
